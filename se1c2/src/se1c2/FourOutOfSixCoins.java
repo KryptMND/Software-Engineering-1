3498274ea490f70b0104adf6067862726e1031bf
Malte <malte.neuendorf@yahoo.de>
2021-11-03 18:33:11 +0100

    Software Engineering 1 Versuch 2

---
+++ se1c2/src/se1c2/FourOutOfSixCoins.java
@@ -0,0 +1,20 @@
+package se1c2;
+
+public class FourOutOfSixCoins {
+    public static void main(String[] args) throws Exception {
+        int count = 0;
+
+        for(int i=1; i <= 6; i++){
+            for(int j=1; j <= 6; j++){
+                for(int k=1; k <= 6; k++){
+                    for(int l=1; l <= 6; l++){
+                        System.out.println(i + ", " + j + ", " + k + ", " + l);
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
+
